85ea5cb4e5bfe79e2da1722675d24d4e03e4a3af
HassanAmed <hassan4621@gmail.com>
2020-03-28 22:01:21 +0500

    connect mysql

--- scrape.js
+++ scrape.js
@@ -1,33 +1,67 @@
 const request = require('request');
 const cheerio = require('cheerio');
+var mysql = require('mysql');
 let allData = [];
 let itemsArray = [];
-// constructor
+
+//DB Connection
+var db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "12345",
+    database: 'stores',
+});
+
+//Connect
+db.connect((err) => {
+    if (err) throw err;
+    console.log("MYSQL Connected");
+    db.query("CREATE DATABASE IF NOT EXISTS stores", (err, result) => {
+        if (err) throw err;
+        console.log("Database Created")
+    });
+});
+var sql = "CREATE TABLE IF NOT EXISTS products (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255), price VARCHAR(255), store VARCHAR(255), pricePerWeight VARCHAR(255), imageUrl VARCHAR(255), category VARCHAR(255), availability BOOLEAN)";
+//Create Tesco Table
+db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Products Table created");
+  });
+function dataEntry(values){
+        var sql = "INSERT INTO products (title, price, store, pricePerWeight, imageUrl, category, availability) VALUES ?";
+        db.query(sql, [values], function (err, result) {
+          if (err) throw err;
+          console.log("Number of records inserted: " + result.affectedRows);
+        });
+}
+
+  // constructor
 function Item() {
     this.title = undefined;
     this.price = null;
+    this.store = 'Tesco';
     this.pricePerWeight = null;
     this.imageUrl = null;
     this.category = undefined;
-    this.available = 'No';
+    this.available = false;
 }
 // used to avoid redundant code as url follow same pattren except only category name + number of pages for each category
 let categories = [
-    { name: 'fresh-food', pages: 52 },
-    { name: 'bakery', pages: 11 },
-    { name: 'frozen-food', pages: 14 },
-    { name: 'food-cupboard', pages: 105 },
-    { name: 'drinks', pages: 50 },
-    { name: 'baby', pages: 15 },
-    { name: 'health-and-beauty', pages: 66 },
-    { name: 'pets', pages: 12 },
-    { name: 'household', pages: 15 },
-    { name: 'home-and-ents', pages: 72 },
-    { name: 'easter', pages: 13 }
+    { name: 'fresh-food', pages: 1 },
+    // { name: 'bakery', pages: 1 },
+    // { name: 'frozen-food', pages: 14 },
+    // { name: 'food-cupboard', pages: 105 },
+    // { name: 'drinks', pages: 50 },
+    // { name: 'baby', pages: 15 },
+    // { name: 'health-and-beauty', pages: 66 },
+    // { name: 'pets', pages: 12 },
+    // { name: 'household', pages: 15 },
+    // { name: 'home-and-ents', pages: 72 },
+    // { name: 'easter', pages: 13 }
 ]
 for (let itr = 0; itr < categories.length; itr++) {
     for (let i = 1; i <= categories[itr].pages; i++) {
-        request(`https://www.tesco.com/groceries/en-GB/shop/${categories[itr].name}/all?page=${i}&count=48`, (error, response, html) => {
+        request(`https://www.tesco.com/groceries/en-GB/shop/${categories[itr].name}/all?page=${i}&count=24`, (error, response, html) => {
             if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
                 $('.product-list--list-item').each((i, el) => {
@@ -41,7 +75,7 @@ for (let itr = 0; itr < categories.length; itr++) {
                         .replace(/\s\s+/g, '');
                     if (price) {
                         item.price = `£${price}`;
-                        item.available = 'Yes'
+                        item.available = true;
                     }
                     const pricePerWeightValue = $(el).find('.price-per-quantity-weight .value')
                         .text()
@@ -54,12 +88,15 @@ for (let itr = 0; itr < categories.length; itr++) {
                         .attr('src')
                     if (imageUrl) item.imageUrl = imageUrl;
                     item.category = categories[itr].name
-
-                    itemsArray[i] = item;
+                    let itemData = [];
+                    for (let key in item){
+                        itemData.push(item[key]);
+                    }
+                    itemsArray[i] = itemData;
                 });
                 allData = [...itemsArray, ...allData];
-                // console.log(allData);
-                // console.log(allData.length);
+                dataEntry(allData);
+                console.log(allData);
             }
         });
     }
